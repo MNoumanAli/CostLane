537c6d8e8995387df32aab54bb6a636ea6089ffe
MNoumanAli <mnoumanali001@gmail.com>
2024-07-01 15:06:23 +0500

    setup server routes

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,15 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from "./config/db.js"
+import router from "./routes/index.js"
 const app = express()
 
 dotenv.config()
 app.use(cors())
+
+app.use('/', router.userRourter);
+app.use('/expense', router.expenseRouter);
+app.use('/dailyExpense', router.dailyExpenseRouter);
+app.use('/interval', router.intervalRouter)
+
 connectDB(app);
