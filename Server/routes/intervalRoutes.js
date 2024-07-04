a729146f51eb39d309816379bef1c8d85cdecf5b
MNoumanAli <mnoumanali001@gmail.com>
2024-07-04 15:39:32 +0500

    removed the typo

--- Server/routes/intervalRoutes.js
+++ Server/routes/intervalRoutes.js
@@ -6,10 +6,10 @@ import intervalSchema from "../schemas/intervalSchema";
 const intervalRouter = express.Router();
 const validateIntervalSchema = validateSchema(intervalSchema);
 
-intervalRoutes.get('/get', authentication, (req,res)=> {res.status(200).send({});});
-intervalRoutes.get('/get/all', authentication, (req,res)=> {res.status(200).send({});});
-intervalRoutes.post('/post', authentication, validateIntervalSchema, (req,res)=> {res.status(200).send({});});
-intervalRoutes.put('/update/:id', authentication, (req,res)=> {res.status(200).send({});});
-intervalRoutes.delete('/delete/:id', authentication, (req,res)=> {res.status(200).send({});});
+intervalRouter.get('/get', authentication, (req,res)=> {res.status(200).send({});});
+intervalRouter.get('/get/all', authentication, (req,res)=> {res.status(200).send({});});
+intervalRouter.post('/post', authentication, validateIntervalSchema, (req,res)=> {res.status(200).send({});});
+intervalRouter.put('/update/:id', authentication, (req,res)=> {res.status(200).send({});});
+intervalRouter.delete('/delete/:id', authentication, (req,res)=> {res.status(200).send({});});
 
 export default intervalRouter;
